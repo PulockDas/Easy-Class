7f019cb46401864c98b2ef88b4c38abdb0340e20
PulockDas <pulock46@student.sust.edu>
2019-10-15 11:18:34 +0600

    Class cancel button added

--- app/src/main/java/com/example/teacher/SecondActivity.java
+++ app/src/main/java/com/example/teacher/SecondActivity.java
@@ -8,9 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 
+import com.example.teacher.Teacher_3rd_Activity.CancelClass;
+import com.example.teacher.Teacher_3rd_Activity.Extra_Class;
+import com.example.teacher.Teacher_3rd_Activity.Routine;
+
 public class SecondActivity extends AppCompatActivity {
 
     private Button exams;
+    private Button routine;
+    private Button extra_class;
+    private Button cancel_class;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +26,44 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         exams = (Button) findViewById(R.id.pick_date);
+        routine = (Button) findViewById(R.id.change_time);
+        extra_class = (Button) findViewById(R.id.extraClass);
+        cancel_class = (Button) findViewById(R.id.cancelClass);
+
+
 
         exams.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent2 = new Intent(SecondActivity.this, Date_picker.class);
+                startActivity(intent2);
+            }
+        });
+
+        routine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(SecondActivity.this, Routine.class);
+                startActivity(intent2);
+            }
+        });
+
+        extra_class.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(SecondActivity.this, Extra_Class.class);
+                startActivity(intent2);
+            }
+        });
+
+        cancel_class.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(SecondActivity.this, CancelClass.class);
                 startActivity(intent2);
             }
         });
---
+++ app/src/main/java/com/example/teacher/Teacher_3rd_Activity/CancelClass.java
@@ -0,0 +1,16 @@
+package com.example.teacher.Teacher_3rd_Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teacher.R;
+
+public class CancelClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_class);
+    }
+}
